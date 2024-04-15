4836f59b23b710f0d4387c7a63fa13e70e2e9336
marufhossain55 <engrmaruf3@gmail.com>
2024-04-15 21:06:11 +0600

    login added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,8 @@
+import { NavLink } from 'react-router-dom';
+
 const Login = () => {
   return (
-    <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 dark:bg-gray-50 dark:bg-gray-50 dark:text-gray-800 dark:text-gray-800'>
+    <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 dark:bg-gray-50 dark:bg-gray-50 dark:text-gray-800 dark:text-gray-800 mx-auto'>
       <div className='mb-8 text-center'>
         <h1 className='my-3 text-4xl font-bold'>Sign in</h1>
         <p className='text-sm dark:text-gray-600 dark:text-gray-600'>
@@ -17,7 +19,7 @@ const Login = () => {
               type='email'
               name='email'
               id='email'
-              placeholder='leroy@jenkins.com'
+              placeholder='email'
               className='w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:border-gray-300 dark:bg-gray-50 dark:bg-gray-50 dark:text-gray-800 dark:text-gray-800'
             />
           </div>
@@ -47,20 +49,21 @@ const Login = () => {
           <div>
             <button
               type='button'
-              className='w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:bg-violet-600 dark:text-gray-50 dark:text-gray-50'
+              className='btn w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:bg-violet-600 dark:text-gray-50 dark:text-gray-50'
             >
               Sign in
             </button>
           </div>
           <p className='px-6 text-sm text-center dark:text-gray-600 dark:text-gray-600'>
             Don't have an account yet?
-            <a
+            <NavLink
+              to='/register'
               rel='noopener noreferrer'
               href='#'
-              className='hover:underline dark:text-violet-600 dark:text-violet-600'
+              className='hover:underline font-bold dark:text-violet-600 dark:text-violet-600'
             >
               Sign up
-            </a>
+            </NavLink>
             .
           </p>
         </div>
